1277651f7b2b3be195cb69784d711da6a05a39bb
Zubenna <emelunna@gmail.com>
2021-02-16 10:18:01 +0100

    Initial Set Up Of Game And Title Scene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,16 +1,60 @@
-import 'phaser';
- 
+import Phaser from 'phaser';
+import { gameOptions, gameConfig } from '../Config/config';
+
+
 export default class GameScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Game');
   }
- 
-  preload () {
-    // load images
-    this.load.image('logo', 'assets/logo.png');
+
+  init() {
+    this.gameOver = false;
+    this.score = 0;
+    this.scoreText = null;
   }
- 
-  create () {
-    this.add.image(400, 300, 'logo');
+
+  create() {
+    this.mountainGroup = this.add.group();
+
+    this.platformGroup = this.add.group({
+      removeCallback(platform) {
+        platform.scene.platformPool.add(platform);
+      },
+    });
+
+    this.platformPool = this.add.group({
+      removeCallback(platform) {
+        platform.scene.platformGroup.add(platform);
+      },
+    });
+
+    this.coinGroup = this.add.group({
+      removeCallback(coin) {
+        coin.scene.coinPool.add(coin);
+      },
+    });
+
+    this.coinPool = this.add.group({
+      removeCallback(coin) {
+        coin.scene.coinGroup.add(coin);
+      },
+    });
+
+    this.fireGroup = this.add.group({
+
+      removeCallback(fire) {
+        fire.scene.firePool.add(fire);
+      },
+    });
+
+    this.firePool = this.add.group({
+      removeCallback(fire) {
+        fire.scene.fireGroup.add(fire);
+      },
+    });
+
+  } 
+  update() {
+    
   }
-};
+}
