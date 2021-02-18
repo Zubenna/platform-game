041690312bc9f6bb212ab00d39f7b239f3d00faf
Zubenna <emelunna@gmail.com>
2021-02-18 17:26:44 +0100

    Adding Physics, Logic and Features Of the Platform

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -78,25 +78,127 @@ export default class GameScene extends Phaser.Scene {
       }, null, this);
 
       this.physics.add.overlap(this.player, this.coinGroup, function func2(player, coin) {
-        this.pickupMusic = this.sound.add('pickup', { volume: 0.5, loop: false });
-        this.pickupMusic.play();
-        this.tweens.add({
-          targets: coin,
-          y: coin.y - 100,
-          alpha: 0,
-          duration: 800,
-          ease: 'Cubic.easeOut',
-          callbackScope: this,
-          onComplete() {
-            this.score += 1;
-            this.scoreText.setText(`Score: ${this.score}`);
-            this.coinGroup.killAndHide(coin);
-            this.coinGroup.remove(coin);
+      this.pickupMusic = this.sound.add('pickup', { volume: 0.5, loop: false });
+      this.pickupMusic.play();
+      this.tweens.add({
+        targets: coin,
+        y: coin.y - 100,
+        alpha: 0,
+        duration: 800,
+        ease: 'Cubic.easeOut',
+        callbackScope: this,
+        onComplete() {
+          this.score += 1;
+          this.scoreText.setText(`Score: ${this.score}`);
+          this.coinGroup.killAndHide(coin);
+          this.coinGroup.remove(coin);
           },
         });
       }, null, this);
-// Commit one
+      this.physics.add.overlap(this.player, this.fireGroup, function func3() {
+        this.dieMusic = this.sound.add('dead', { volume: 0.5, loop: false });
+        this.dieMusic.play();
+        this.dying = true;
+        this.player.anims.play('dead', false);
+        this.player.body.setVelocityY(-200);
+        this.physics.world.removeCollider(this.platformCollider);
+      }, null, this);
 
+      this.input.on('pointerdown', this.jump, this);
 
+      this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  }
+
+  addMountains() {
+  const rightmostMountain = this.getRightmostMountain();
+  if (rightmostMountain < gameConfig.width * 2) {
+    const mountain = this.physics.add.sprite(rightmostMountain + Phaser.Math.Between(100, 350), gameConfig.height + Phaser.Math.Between(0, 100), 'mountain');
+    mountain.setOrigin(0.5, 1);
+    mountain.body.setVelocityX(gameOptions.mountainSpeed * -1);
+    this.mountainGroup.add(mountain);
+      if (Phaser.Math.Between(0, 1)) {
+        mountain.setDepth(1);
+      }
+    mountain.setFrame(Phaser.Math.Between(0, 3));
+    this.addMountains();
     }
+  }
+
+  getRightmostMountain() {
+  let rightmostMountain = -200;
+  this.mountainGroup.getChildren().forEach((mountain) => {
+  rightmostMountain = Math.max(rightmostMountain, mountain.x);
+  });
+    return rightmostMountain;
+  }
+
+  addPlatform(platformWidth, posX, posY) {
+    this.addedPlatforms += 1;
+    let platform;
+    if (this.platformPool.getLength()) {
+      platform = this.platformPool.getFirst();
+      platform.x = posX;
+      platform.y = posY;
+      platform.active = true;
+      platform.visible = true;
+      this.platformPool.remove(platform);
+      platform.displayWidth = platformWidth;
+      platform.tileScaleX = 1 / platform.scaleX;
+    } else {
+      platform = this.add.tileSprite(posX, posY, platformWidth, 32, 'platform');
+      this.physics.add.existing(platform);
+      platform.body.setImmovable(true);
+      platform.body.setVelocityX(Phaser.Math.Between(
+        gameOptions.platformSpeedRange[0],
+        gameOptions.platformSpeedRange[1],
+      ) * -1);
+      platform.setDepth(2);
+      this.platformGroup.add(platform);
+    }
+    this.nextPlatformDistance = Phaser.Math.Between(
+      gameOptions.spawnRange[0],
+      gameOptions.spawnRange[1],
+    );
+
+    if (this.addedPlatforms > 1) {
+      if (Phaser.Math.Between(1, 100) <= gameOptions.coinPercent) {
+        if (this.coinPool.getLength()) {
+          const coin = this.coinPool.getFirst();
+          coin.x = posX;
+          coin.y = posY - 96;
+          coin.alpha = 1;
+          coin.active = true;
+          coin.visible = true;
+          this.coinPool.remove(coin);
+        } else {
+          const coin = this.physics.add.sprite(posX, posY - 96, 'coin');
+          coin.setImmovable(true);
+          coin.setVelocityX(platform.body.velocity.x);
+          coin.anims.play('rotate');
+          coin.setDepth(2);
+          this.coinGroup.add(coin);
+        }
+      }
+
+      if (Phaser.Math.Between(1, 100) <= gameOptions.firePercent) {
+        if (this.firePool.getLength()) {
+          const fire = this.firePool.getFirst();
+          fire.x = posX - platformWidth / 2 + Phaser.Math.Between(1, platformWidth);
+          fire.y = posY - 46;
+          fire.alpha = 1;
+          fire.active = true;
+          fire.visible = true;
+          this.firePool.remove(fire);
+        } else {
+          const fire = this.physics.add.sprite(posX - platformWidth / 2 + Phaser.Math.Between(1, platformWidth), posY - 46, 'fire');
+          fire.setImmovable(true);
+          fire.setVelocityX(platform.body.velocity.x);
+          fire.setSize(8, 2, true);
+          fire.anims.play('burn');
+          fire.setDepth(2);
+          this.fireGroup.add(fire);
+        }
+      }
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
 import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
-// import OptionsScene from './Scenes/OptionsScene';
+import OptionsScene from './Scenes/OptionsScene';
 // import CreditsScene from './Scenes/CreditsScene';
 // import Model from './Model';
 // import LoginScene from './Scenes/LoginScene';
@@ -14,17 +14,17 @@ import TitleScene from './Scenes/TitleScene';
 class Game extends Phaser.Game {
   constructor() {
     super(gameConfig);
-    const model = new Model();
-    this.globals = { model, bgMusic: null };
+    // const model = new Model();
+    // this.globals = { model, bgMusic: null };
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Login', LoginScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Options', OptionsScene);
-    this.scene.add('Credits', CreditsScene);
+    // this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
-    this.scene.add('GameOver', GameOverScene);
-    this.scene.add('LeaderBoard', leaderBoardScene);
+    // this.scene.add('GameOver', GameOverScene);
+    // this.scene.add('LeaderBoard', leaderBoardScene);
     this.scene.start('Boot');
   }
 }
